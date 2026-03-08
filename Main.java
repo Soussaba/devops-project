1864fee80ddf52cd7c4238f4935fa213e7da9a2b
SOUSSA <basoussa0@gmail.com>
2026-03-08 18:53:59 +0100

    Fix Main.java contenu

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("DevOps Pipeline fonctionne !");
+        System.out.println("Build Java reussi !");
+        System.out.println("Jenkins + Github + Java = CI/CD OK !");
+    }
+}
